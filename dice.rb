799d918d293fe960620b10b151641e19356d8249
Thierry Chau <thierry.chau@gmail.com>
2023-07-24 20:52:39 +0000

    Added better_errors, nav links, and dice rolls

--- dice.rb
+++ dice.rb
@@ -2,6 +2,13 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "<h1>Dice Roll</h1>
@@ -27,7 +34,7 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  outcome = "You rolled a #{first_due} and a #{second_die} for a total of #{sum}."
 	
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
